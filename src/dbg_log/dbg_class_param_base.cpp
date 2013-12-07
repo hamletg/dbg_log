0d41d9e4de4758e844956db941922f593a61b292
Michel Gillet <michel.r.gillet@gmail.com>
2013-12-07 15:48:18 +0200

    Adds a pointer to dbg_class to each parameter.

--- src/dbg_log/dbg_class_param_base.cpp
+++ src/dbg_log/dbg_class_param_base.cpp
@@ -6,15 +6,15 @@ namespace dbg_log
 {
 
 dbg_class_param_base::dbg_class_param_base(char *name,bool output)
-    :m_name(name),m_output_param(output),m_is_array(false),m_array_len(0)
+    : m_name(name), m_output_param(output), m_is_array(false), m_array_len(0), m_dbg_class(0)
 {}
 
 dbg_class_param_base::dbg_class_param_base(const char *name,bool output)
-    :m_name(name),m_output_param(output),m_is_array(false),m_array_len(0)
+    : m_name(name), m_output_param(output), m_is_array(false), m_array_len(0), m_dbg_class(0)
 {}
 
 dbg_class_param_base::dbg_class_param_base(char *name,int len,bool output)
-    :m_name(name),m_output_param(output),m_is_array(true),m_array_len(len)
+    : m_name(name), m_output_param(output), m_is_array(true), m_array_len(len), m_dbg_class(0)
 {}
 
 dbg_class_param_base::~dbg_class_param_base()
@@ -31,6 +31,16 @@ bool dbg_class_param_base::IsOutput()
 void dbg_class_param_base::SetOuput(bool value)
 {
     m_output_param=value;
+}
+
+void dbg_class_param_base::SetDbgClass(dbg_class *dbgclass)
+{
+    m_dbg_class=dbgclass;
+}
+
+dbg_class *dbg_class_param_base::GetDbgClass()
+{
+    return m_dbg_class;
 }
 
 }
