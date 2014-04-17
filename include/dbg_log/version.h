6fd2845b6d3476d0b3dd53a47eab7e1db1ec10b8
Michel Gillet <michel.r.gillet@gmail.com>
2014-04-17 07:57:32 +0300

    Bumps version number up.

--- include/dbg_log/version.h
+++ include/dbg_log/version.h
@@ -16,12 +16,12 @@
 // Bump-up with each new version
 #define DBG_LOG_MAJOR_VERSION    0
 #define DBG_LOG_MINOR_VERSION    0
-#define DBG_LOG_RELEASE_NUMBER   1
-#define DBG_LOG_VERSION_STRING   _T("dbg_log 0.0.1")
+#define DBG_LOG_RELEASE_NUMBER   2
+#define DBG_LOG_VERSION_STRING   _T("dbg_log 0.0.2")
 
 // Must be updated manually as well each time the version above changes
-#define DBG_LOG_VERSION_NUM_DOT_STRING   "0.0.1"
-#define DBG_LOG_VERSION_NUM_STRING       "0001"
+#define DBG_LOG_VERSION_NUM_DOT_STRING   "0.0.2"
+#define DBG_LOG_VERSION_NUM_STRING       "0002"
 
 // nothing should be updated below this line when updating the version
 
